e4237dff44a1c706e0a986645ce50c39b9d50aae
马添翼 <matianyi@jtoushou.com>
2017-12-19 17:06:42 +0800

    init project

---
+++ src/main/java/cn/mty/jts/jtsoa/JtsOaApplication.java
@@ -0,0 +1,12 @@
+package cn.mty.jts.jtsoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtsOaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtsOaApplication.class, args);
+	}
+}
